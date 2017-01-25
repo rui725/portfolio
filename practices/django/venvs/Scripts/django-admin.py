850626e26c7952939a19c46f13b778d37b5840a6
rui rafael rosillas <rui rafael rosillas>
2017-01-24 20:20:42 -0800

    Added music application

---
+++ practices/django/venvs/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Rui\Desktop\stuff\rui-portf\practices\django\venvs\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

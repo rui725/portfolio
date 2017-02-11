84490fcc6f42667f470932939fbfaabff15ff84c
rui rafael rosillas <rui rafael rosillas>
2017-02-11 12:21:56 -0800

    added tutorial 1-5 from thenewboston

---
+++ practices/nodeJS/app/app/tut1.js
@@ -0,0 +1,13 @@
+﻿// returns undefined when it is empty function
+function person() {
+}
+// called anonymous function
+var printRui = function () {
+    console.log('Gwapo ba');
+}
+
+printRui();
+
+// calls the function within 5seconds
+setTimeout(printRui, 5000);
+
---
+++ practices/nodeJS/app/app/tut4.js
@@ -0,0 +1,19 @@
+﻿// this is an object
+var Rui = {
+    printMe: function () {
+        console.log("Hi I'm Rui");
+        console.log(this === Rui); //returns true becoz the this keyword basically refers to the one calling it
+        console.log(this === global); // returns false becoz this was not called using global
+    }
+
+}
+
+Rui.printMe(); // printMe was called through Rui 
+
+function DoSomething() {
+    console.log("I'm doing something");
+    console.log(this === global); // returns true becoz it was called in global
+    console.log(this === Rui); // returns false becoz it was not called using Rui
+
+}
+DoSomething(); //called using global

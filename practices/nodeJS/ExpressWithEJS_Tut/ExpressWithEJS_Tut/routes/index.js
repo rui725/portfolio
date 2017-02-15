317c734f2b7d479ba1d1abcf72f14dd77b65e8ed
rui rafael rosillas <rui rafael rosillas>
2017-02-14 17:52:40 -0800

    finished server tut working on EXPRESS with EJS

---
+++ practices/nodeJS/ExpressWithEJS_Tut/ExpressWithEJS_Tut/routes/index.js
@@ -0,0 +1,9 @@
+﻿var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res) {
+    res.render('index', { title: 'Express' ,name:'Rui Rafael Rosillas'});
+});
+
+module.exports = router;

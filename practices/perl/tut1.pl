d209145982b279468742744ae86c3774271462f0
rui rafael rosillas <rui rafael rosillas>
2017-05-02 21:53:41 -0700

    modified tut1.pl to add escapes sequences, basic comments and difference between double and single quote

--- practices/perl/tut1.pl
+++ practices/perl/tut1.pl
@@ -1,1 +1,27 @@
+# comments in PERL 
+# basics of PERL
+
+#prints hello world
 print 'HI';
+
+$myVar = 25; # this is how you call a variable 
+
+
+# difference between single and double quotation
+print "Hi I am Rui $myVar \n"; # outputs the value of myVar
+print 'Hi I am Rui $myVar \n'; # outputs literal word "myVar" and escape sequence such as \n
+
+# escape sequences
+#
+# \n new line
+# \" puts " in to the string
+# \a alert beeps
+# \t tabs 5 spaces
+# \u makes the next character into uppercase
+# \l makes the next character into lowercase
+# \U makes all the following character into Uppercase
+# \L makes all the following character into lowercase
+# \+!@$ escapes all the alpha numeric special characters
+
+
+

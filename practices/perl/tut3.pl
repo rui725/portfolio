1bb89ccf7ff6c2d494a2769b2cbe89efc67710e2
rui rafael rosillas <rui rafael rosillas>
2017-05-03 17:29:53 -0700

    add lesson on hash using perl

--- practices/perl/tut3.pl
+++ practices/perl/tut3.pl
@@ -23,3 +23,21 @@ print "name: @name\n";
 splice (@name,1,1,@rep); # splice(arrayToChange,startIndex,Length, arrayValueToSet)
 print "name: @name\n";
 
+#merging array into one string
+@games = ("LOL","DOTA2", "CS", "TEKKEN");
+#join(delimeter, array)
+$mstring = join("-",@games); # - is delimiter to combine the string in array
+print "String: $mstring\n";
+
+#splitting a string to array
+#split(delimiter, string variable);
+@marr=split("-",$mstring);
+print "Array: @marr[1..2]\n";
+
+#SORT ARRAY
+@sorta = sort(@games);
+print "Sorted Array: @sorta\n";
+# mergin array
+@merga = (1..5,6..10);
+print "Merge array: @merga\n";
+

5c84a220679507f60e008e6a0302356e7db60d2e
rui rafael rosillas <rui rafael rosillas>
2017-07-30 13:06:45 -0700

    added lesson 2 and 3

--- practices/bash_scripts/tut2.sh
+++ practices/bash_scripts/tut2.sh
@@ -8,3 +8,23 @@ read name
 
 echo Your name is $name
 
+
+#simple login form using bash
+read -p 'Username:' uname # -p means prompt 
+read -sp 'Password:' upass # -sp means silent input
+
+echo 
+
+echo Thank you $uname for logging in
+
+# Reading Multiple inputs
+echo "Enter games you will buy"
+
+read game1 game2 game3
+
+echo "you inputted $game1"
+echo "you inputted $game2"
+echo you inputted $game3
+
+
+
